1002d34e6c0c8fc0067be57b30ef103b619b2bf7
Will Roden <will@roden.cc>
2022-03-24 09:45:21 -0500

    VARS

--- .github/actions/hellodocker/entrypoint.sh
+++ .github/actions/hellodocker/entrypoint.sh
@@ -3,3 +3,9 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+echo "FOO=$FOO"
+echo "GITHUB_EVENT_NAME=$GITHUB_EVENT_NAME"
+echo "GITHUB_REPOSITORY=$GITHUB_REPOSITORY"
+echo "GITHUB_REF=$GITHUB_REF"
+echo "GITHUB_REF_NAME=$GITHUB_REF_NAME"
+echo "GITHUB_REF_NAME=$GITHUB_REF_TYPE"
